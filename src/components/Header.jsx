32154015e5052be2db1bc5479215d3492baf9f1b
Your Name <your.email@example.com>
2024-12-30 12:29:53 -0500

    updated header copy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
     <div className="  lg:h-[343px] w-100 bg-sectionDark dark:bg-mainBG rounded-lg flex flex-col lg:flex-row p-8 md:justify-between">
       <div className="  p-1 lg:w-1/2 sm:w-full  flex items-center text-grey-0 dark:text-grey-100 ">
         <h1 className=" w-full">
-          A curated list of learning resources for creative.
+          A curated list of learning resources for creatives.
         </h1>
       </div>
       <div className=" flex justify-center md:w-1/3">
